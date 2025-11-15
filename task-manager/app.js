8483039bdfc63936cc18cb9daf330f149efcbeee
massi.brero@gmail.com <massi.brero@gmail.com>
2025-11-15 19:04:06 +0100

    TaskList Filter

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,19 +1,77 @@
-const { createApp, onMounted, onBeforeUnmount, ref } = Vue
+const {
+  createApp,
+  onMounted,
+  onBeforeUnmount,
+  ref,
+  watch,
+  watchEffect,
+  computed,
+} = Vue
 
 const App = {
   setup() {
-    const tasks = ref([
+    const mockTasks = ref([
       { id: 1, title: 'Buy groceries', priority: 'High', done: false },
       { id: 2, title: 'Walk the dog', priority: 'Medium', done: true },
       { id: 3, title: 'Read a book', priority: 'Low', done: false },
     ])
     const newTaskInput = ref(null)
+    const newTaskTitle = ref('')
+    const newPriority = ref('Medium')
+    const filteredText = ref(null)
+    const tasks = ref(JSON.parse(localStorage.getItem('tasks')) ?? mockTasks)
+    const filteredTasks = computed(() => {
+      console.log('hey')
+
+      if (filteredText.value === null || filteredText.value === '') {
+        return tasks.value
+      }
+      return tasks.value.filter((task) =>
+        task.title.toLowerCase().includes(filteredText.value.toLowerCase())
+      )
+    })
+
     onMounted(() => {
       newTaskInput.value.focus()
+      filteredTasks.value = tasks.value
     })
+
+    watch(
+      tasks,
+      () => {
+        localStorage.setItem('tasks', JSON.stringify(tasks.value))
+      },
+      { deep: true }
+    )
+
+    addTask = () => {
+      if (newTaskTitle.value.trim() === '' || newPriority.value.trim() === '') {
+        return
+      }
+
+      const newTask = {
+        id: tasks.value.length + 1,
+        title: newTaskTitle.value,
+        priority: newPriority.value,
+        done: false,
+      }
+      tasks.value.unshift(newTask)
+      resetForm()
+    }
+
+    function resetForm() {
+      newTaskTitle.value = ''
+      newPriority.value = ''
+      newTaskInput.value.focus()
+    }
     return {
       newTaskInput,
       tasks,
+      newTaskTitle,
+      newPriority,
+      addTask,
+      filteredTasks,
+      filteredText,
     }
   },
 }
