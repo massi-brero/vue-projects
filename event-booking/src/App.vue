e580c21e7db591e5e9ec609e96da863eb8f05a59
brero <massimiliano.brero@adesso.de>
2025-11-18 18:08:06 +0100

    1, component

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <main>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+    <h1 class="text-2xl font-medium">All Events</h1>
+    <EventCard />
+    <h1 class="text-2xl font-medium">Yout Bookings</h1>
   </main>
 </template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
--- event-booking/vite.config.js
+++ event-booking/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url' // <-- WICHTIG
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()]
+  plugins: [vue(), tailwindcss(), VueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
