f671a8e24c6da75b1ab696dad1c5d2af2fd38256
brero <massimiliano.brero@adesso.de>
2025-11-20 17:01:22 +0100

    Cards

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,7 +2,16 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h1 class="text-2xl font-medium">All Events</h1>
-    <EventCard />
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conf 2025"
+        when="2025/12/12"
+        description="A Vue Conference"
+        @register="console.log('registered')"
+      />
+    </section>
     <h1 class="text-2xl font-medium">Yout Bookings</h1>
   </main>
 </template>
--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>Component</div>
+  <div class="border border-gray-400 bg-white rounded-md p-2">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-200">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+    <section class="flex justify-end">
+      <button
+        class="text-small font-medium border border-gray-400 rounded-full px-3 py-1 hover:bg-gray-100 cursor-pointer"
+        @click="$emit('register')"
+      >
+        Register
+      </button>
+    </section>
+  </div>
 </template>
 
-<script>
-export default {}
+<script setup>
+defineProps({ title: String, when: String, description: String })
+defineEmits(['register'])
 </script>
 
 <style lang="scss" scoped></style>
