efa90403536bee885037175adb32f482fd72c243
massi.brero@gmail.com <massi.brero@gmail.com>
2025-11-25 18:11:14 +0100

    loader

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,18 +2,20 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h1 class="text-2xl font-medium">All Events</h1>
-    <section class="grid grid-cols-2 gap-8" v-if="!loading">
-      <EventCard
-        v-for="e in events"
-        :key="e.id"
-        :title="e.title"
-        :when="e.date"
-        :description="e.description"
-        @register="console.log('registered')"
-      />
-    </section>
-    <section v-else>
-      <p>Loading...</p>
+    <section class="grid grid-cols-2 gap-8" x>
+      <template v-if="!loading">
+        <EventCard
+          v-for="e in events"
+          :key="e.id"
+          :title="e.title"
+          :when="e.date"
+          :description="e.description"
+          @register="console.log('registered')"
+        />
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i" />
+      </template>
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
@@ -27,6 +29,7 @@ import BookingItem from '@/components/BookingItem.vue'
 import EventCard from '@/components/EventCard.vue'
 import dataService from '@/services/data-service.js'
 import { onMounted, ref } from 'vue'
+import LoadingEventCard from './components/LoadingEventCard.vue'
 
 const events = ref([])
 const loading = ref(false)
