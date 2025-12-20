472f1ced7654142bac93b32c37457ec0db6719ea
massi.brero@gmail.com <massi.brero@gmail.com>
2025-12-20 20:15:52 +0100

    dynamic components

--- event-booking/src/components/BookingItem.vue
+++ event-booking/src/components/BookingItem.vue
@@ -3,7 +3,9 @@
     <div class="flex justify-between items-center">
       <div class="flex space-x-2">
         <div>{{ title }}</div>
-        <div>{{ status }}</div>
+        <div>
+          <component :is="icon" :class="pending ? 'animate-spin' : 'text-green-500'"></component>
+        </div>
       </div>
       <RoundButton variant="danger" @click="$emit('cancel')">Cancel</RoundButton>
     </div>
@@ -13,10 +15,15 @@
 <script setup>
 import SectionCard from '@/components/SectionCard.vue'
 import RoundButton from '@/components/RoundButton.vue'
+import { LoaderCircle, Check } from 'lucide-vue-next'
+import { computed } from 'vue'
 
-defineProps({
+const props = defineProps({
   title: String,
   status: String
 })
+const pending = computed(() => (props.status === 'pending') | (props.status === 'cancelling'))
+const icon = computed(() => (pending.value ? LoaderCircle : Check))
+
 defineEmits(['cancel'])
 </script>
